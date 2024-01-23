adf6b199b794e91d70ef6fe8c8d9079d26097d92
gabriel.kenji <gabriel.kenji@ongame.com.br>
2024-01-23 16:14:20 -0300

    chore(Start.sh): Added --noinput

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PORT=8000
 ADDRESS=0.0.0.0:${PORT}
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 export DJANGO_SETTINGS_MODULE=backend.settings
 echo "DJANGO_SETTINGS_MODULE: $DJANGO_SETTINGS_MODULE"
