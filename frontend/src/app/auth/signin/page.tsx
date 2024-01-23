380ca94d1ca4ea817c3376d9d1494747168aa9b3
gabriel.kenji <gabriel.kenji@ongame.com.br>
2024-01-23 16:08:25 -0300

    fix(Frontend/Auth Page): Adjusted - Removed console.log - Adjusted session

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,40 +1,58 @@
 'use client'
 import { PaperAirplaneIcon } from '@heroicons/react/20/solid';
 import { Card, CardBody, CardHeader, Input, Divider, Button, CardFooter, Link } from '@nextui-org/react';
-import { signIn } from 'next-auth/react';
 import { SyntheticEvent, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
 export default function LoginPage() {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [errorMessage, setErrorMessage] = useState<string[]>([]);
+    const [message, setMessage] = useState<string>('');
 
     const router = useRouter()
     async function handleSubmit (event: SyntheticEvent) {
-        event.preventDefault()
-
-        const res = await signIn('credentials', {
+        axios.post('/api/auth/register/', {
             username: username,
             password: password,
-            redirect: false
+            email: email
+        }).then((res) => {
+            if (res.status === 201) {
+                router.push('/auth/login')
+            }
+        }).catch((error)=>{
+            setErrorMessage(error.response.data)
         })
-        if (res?.error){
-            return
-        }
-        router.replace('/')
     }
 
     return(
         <div className='h-screen w-full justify-center items-center flex'>
             <Card className='bg-primary'>
                 <CardHeader className='mx-auto justify-center font-bold text-secondary'>
-                    Login
+                    Register
                 </CardHeader>
                 <Divider/>
                 <CardBody>
                     <div className='flex flex-col gap-4 p-3'>
+                    {errorMessage && Object.keys(errorMessage).length > 0 && (
+                        <div className='text-red-500'>
+                        {Object.keys(errorMessage).map((key: any) => (
+                            <div key={key}>
+                            <strong>{key}:</strong>
+                            {errorMessage[key].map((error: any, index: any) => (
+                                <p key={index}>{error}</p>
+                            ))}
+                            </div>
+                        ))}
+                        </div>
+                    )}
                         <Input color='secondary' variant='underlined' type='text' name='username' label='Username'
                             onChange={(e) => setUsername(e.target.value)}
+                        ></Input>
+                        <Input color='secondary' variant='underlined' type='text' name='email' label='Email'
+                            onChange={(e) => setEmail(e.target.value)}
                         ></Input>
                         <Input color='secondary' variant='underlined' type='password' name='password' label='Password'
                             onChange={(e) => setPassword(e.target.value)}
@@ -43,14 +61,15 @@ export default function LoginPage() {
                         className='hover:font-bold hove:scale-125' 
                         type='submit' 
                         onClick={handleSubmit}
-                        >Login<PaperAirplaneIcon className='h-4 w-4 group-hover:translate-x-8 group-hover:delay-75 ease-in duration-300'/></Button>
+                        >
+                            Cadastrar
+                            <PaperAirplaneIcon className='h-4 w-4 group-hover:translate-x-8 group-hover:delay-75 ease-in duration-300'/>
+                        </Button>
                     </div>
                 </CardBody>
                 <Divider/>
                 <CardFooter className='flex h-5 items-center space-x-4 text-small p-2'>
-                    <Link className='text-sm mx-auto' href='/auth/signin' color='secondary'>Registrar</Link>
-                    <Divider orientation='vertical'/>
-                    <Link className='text-sm' href='/auth/forgot-password' color='secondary'>Esqueci a senha?</Link>
+                    <Link className='text-sm mx-auto' href='/auth/login' color='secondary'>Ja tenho uma conta</Link>
                 </CardFooter>
             </Card>
         </div>
