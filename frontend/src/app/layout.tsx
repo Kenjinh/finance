a9bef0b177c51b43a7f31c4c26f70a42775c8a83
gabriel.kenji <gabriel.kenji@ongame.com.br>
2024-01-23 16:13:13 -0300

    fix(Frontend/Home): Removed session

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from '@/lib/NextUIProvider'
 import Header from "@/components/header/Header"
-import { getServerSession } from 'next-auth'
-import { nextAuthOption } from '@/lib/auth'
 import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,10 +17,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const session = getServerSession(nextAuthOption)
-  if(!session){
-    redirect('/auth/login')
-  }
   return (
     <html lang="pt-Br">
       <body className={inter.className}>
