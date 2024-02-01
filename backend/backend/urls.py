c3d5296bca05e10a452459bf17cc7e37b981b477
gabriel.kenji <nohara0312@gmail.com>
2024-02-01 16:16:59 -0300

    feat(Backend): Added revenues on urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/', include('accounts.urls')),
-    path('api/expense/', include('expenses.urls'))
+    path('api/expense/', include('expenses.urls')),
+    path('api/revenue/', include('revenue.urls')),
 ]
