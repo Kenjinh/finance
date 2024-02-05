bb88d8e4410cf9b313ed55b9a9285e62f8c1c8ae
gabriel.kenji <nohara0312@gmail.com>
2024-02-05 17:01:51 -0300

    feat(Frontend): Add icons on card

--- frontend/src/app/dashboard/_components/dashboard-cards.tsx
+++ frontend/src/app/dashboard/_components/dashboard-cards.tsx
@@ -1,4 +1,5 @@
-import { BanknotesIcon } from "@heroicons/react/20/solid";
+import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/16/solid";
+import { BanknotesIcon, WalletIcon } from "@heroicons/react/20/solid";
 import { CardBody, Card, CardHeader } from "@nextui-org/react";
 
 export default function DashboardCards({ totalExpenses, totalRevenue }: { totalExpenses: number, totalRevenue: number }) {
@@ -8,24 +9,38 @@ export default function DashboardCards({ totalExpenses, totalRevenue }: { totalE
         <Card>
           <CardHeader className="flex justify-center">Total Gasto</CardHeader>
           <CardBody className="flex flex-row gap-2 items-center">
-            <BanknotesIcon className="text-danger w-8 h-8" />
+            <div>
+              <BanknotesIcon className="text-danger w-8 h-8" />
+              <ArrowDownIcon className="text-danger w-4 h-4" />
+            </div>
             <h1 className="text-danger">R$ {totalExpenses}</h1>
           </CardBody>
         </Card>
         <Card>
           <CardHeader className="flex justify-center">Total Receita</CardHeader>
           <CardBody className="flex flex-row gap-2 items-center">
-            <BanknotesIcon className="text-success w-8 h-8" />
+            <div>
+              <BanknotesIcon className="text-success w-8 h-8" />
+              <ArrowUpIcon className="text-success w-4 h-4" />
+            </div>
             <h1 className="text-success">R$ {totalRevenue}</h1>
           </CardBody>
         </Card>
         <Card>
           <CardHeader className="flex justify-center">Saldo</CardHeader>
           <CardBody className="flex flex-row gap-2 items-center">
-            <BanknotesIcon
-              className={Number(totalExpenses) + Number(totalRevenue) < 0 ? "text-danger w-8 h-8" : "text-success w-8 h-8"} />
+            <WalletIcon
+              className={
+                Number(totalExpenses) + Number(totalRevenue) < 0 ?
+                  "text-danger w-8 h-8"
+                  : (Number(totalRevenue) * 0.02) >= Number(totalExpenses) + Number(totalRevenue) ?
+                    "text-warning w-8 h-8" : "text-success w-8 h-8"} />
             <h1
-              className={Number(totalExpenses) + Number(totalRevenue) < 0 ? "text-danger" : "text-success"}>R$ {Number(totalExpenses) + Number(totalRevenue)}</h1>
+              className={
+                Number(totalExpenses) + Number(totalRevenue) < 0 ?
+                  "text-danger"
+                  : (Number(totalRevenue) * 0.02) >= Number(totalExpenses) + Number(totalRevenue) ?
+                    "text-warning" : "text-success"}>R$ {Number(totalExpenses) + Number(totalRevenue)}</h1>
           </CardBody>
         </Card>
       </CardBody>
